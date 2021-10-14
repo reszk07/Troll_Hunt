09e83ac43e9c6365713cca86c9fe911a4a32b213
jolar <jolar@JOLA>
2021-10-14 13:19:26 -0400

    Add project files.

---
+++ Troll_Hunt/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class Troll_Hunt
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(s => int.Parse(s)).ToArray();
+        Console.WriteLine(Math.Ceiling((nums[0] - 1) / (double)(nums[1] / nums[2])));
+    }
+}
